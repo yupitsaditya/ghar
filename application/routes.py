71cbf4ca1cfcd8f82e51facde6451db7a348cd3a
mitesh singh <singhmitesh19@gmail.com>
2020-04-09 14:52:40 +0530

    Added requirements.txt and gitignore

--- application/routes.py
+++ application/routes.py
@@ -3,6 +3,9 @@ from flask import render_template
 
 @app.route("/")
 @app.route("/index")
-@app.route("/home")
 def index():
     return render_template("index.html", index=True )
+
+@app.route("/home")
+def home():
+    return render_template("home.html", index=True )
